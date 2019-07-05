6d55344c9bf929c3d369d60098e9ffc939d454a3
xiangyong <@>
2019-07-05 10:53:14 +0800

    添加动画

--- app/src/main/java/com/tw/music/Activity.java
+++ app/src/main/java/com/tw/music/Activity.java
@@ -2,7 +2,14 @@ package com.tw.music;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.PixelFormat;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -27,15 +34,42 @@ public class Activity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().setFormat(PixelFormat.TRANSLUCENT); //状态栏透明
+        fullScreen();
         mViewPager = findViewById(R.id.viewpager2);
         mAdapter = new ViewPagerFragmentStateAdapter(getSupportFragmentManager());
         mViewPager.setAdapter(mAdapter);
-        mViewPager.setOrientation(ViewPager2.ORIENTATION_VERTICAL);
+        mViewPager.setOrientation(ViewPager2.ORIENTATION_VERTICAL); //控制为上下切换  默认为左右切换
         mViewPager.setPageTransformer(new MyPageTransformer());
         fragments.add(new Fragment1(mViewPager));
         fragments.add(new Fragment2(mViewPager));
     }
 
+    private void fullScreen() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                //5.x开始需要把颜色设置透明，否则导航栏会呈现系统默认的浅灰色
+                Window window = getWindow();
+                View decorView = window.getDecorView();
+                //两个 flag 要结合使用，表示让应用的主体内容占用系统状态栏的空间
+                int option = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+                decorView.setSystemUiVisibility(option);
+                window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+                window.setStatusBarColor(Color.TRANSPARENT);
+                //导航栏颜色也可以正常设置
+                //	                window.setNavigationBarColor(Color.TRANSPARENT);
+            } else {
+                Window window = getWindow();
+                WindowManager.LayoutParams attributes = window.getAttributes();
+                int flagTranslucentStatus = WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS;
+                int flagTranslucentNavigation = WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION;
+                attributes.flags |= flagTranslucentStatus;
+                attributes.flags |= flagTranslucentNavigation;
+                window.setAttributes(attributes);
+            }
+        }
+    }
     class ViewPagerFragmentStateAdapter extends FragmentStateAdapter {
 
 
--- app/src/main/java/com/tw/music/MyPageTransformer.java
+++ app/src/main/java/com/tw/music/MyPageTransformer.java
@@ -1,5 +1,6 @@
 package com.tw.music;
 
+import android.util.Log;
 import android.view.View;
 
 import androidx.core.view.ViewCompat;
@@ -10,25 +11,83 @@ public class MyPageTransformer  implements ViewPager2.PageTransformer {
 
     private static final float ROT_MAX = 20.0f;
     private float mRot;
-
+    private static float MIN_SCALE = 0.75f;
+    private static float MIN_ALPHA = 0.5f;
     /**
      * @param view     滑动中的那个view
      * @param position
      */
     public void transformPage(View view, float position) {
 
+//        int pageWidth = view.getWidth();
+//        if (position < -1) { // [-Infinity,-1)//This page is way off-screen to the left.
+//            view.setAlpha(0);
+//        } else if (position <= 0) { // [-1,0]Use //the default slide transition when moving to the left page
+//            view.setAlpha(1);
+//            view.setTranslationX(0);
+//            view.setScaleX(1);
+//            view.setScaleY(1);
+//        } else if (position <= 1) { // (0,1]// Fade the page out.
+//            view.setAlpha(1 - position);
+//            // Counteract the default slide transition
+//            view.setTranslationX(pageWidth * -position);
+//            // Scale the page down (between MIN_SCALE and 1)
+//            float scaleFactor = MIN_SCALE + (1 - MIN_SCALE)
+//                    * (1 - Math.abs(position));
+//            view.setScaleX(scaleFactor);
+//            view.setScaleY(scaleFactor);
+//        } else { // (1,+Infinity]
+//            // This page is way off-screen to the right.
+//            view.setAlpha(0);
+//
+//        }
+
+
+//        int pageWidth = view.getWidth();
+//        int pageHeight = view.getHeight();
+//        if (position < -1) { // [-Infinity,-1)
+//            // This page is way off-screen to the left.
+//            view.setAlpha(0);
+//        } else if (position <= 1) { // [-1,1]
+//            // Modify the default slide transition to
+//            // shrink the page as well
+//            float scaleFactor = Math.max(MIN_SCALE, 1 - Math.abs(position));
+//            float vertMargin = pageHeight * (1 - scaleFactor) / 2;
+//            float horzMargin = pageWidth * (1 - scaleFactor) / 2;
+//            if (position < 0) {
+//                view.setTranslationX(horzMargin - vertMargin / 2);
+//            } else {
+//                view.setTranslationX(-horzMargin + vertMargin / 2);
+//            }
+//            // Scale the page down (between MIN_SCALE and 1)
+//            view.setScaleX(scaleFactor);
+//            view.setScaleY(scaleFactor);
+//            // Fade the page relative to its size.
+//            view.setAlpha(MIN_ALPHA + (scaleFactor - MIN_SCALE)
+//                    / (1 - MIN_SCALE) * (1 - MIN_ALPHA));
+//        } else { // (1,+Infinity]
+//            // This page is way off-screen to the right.
+//            view.setAlpha(0);
+//        }
 
         //界面不可见
         if (position < -1) {
             ViewCompat.setRotation(view, 0);
 
-        }
-        //界面可见
-        else if (position <= 1) {
+        }   else if (position <= 0) {
             mRot = (ROT_MAX * position);
             ViewCompat.setPivotX(view, view.getMeasuredWidth() * 0.5f);
             ViewCompat.setPivotY(view, view.getMeasuredHeight());
             ViewCompat.setRotation(view, mRot);
+            Log.i("md","1111111111111");
+        }
+        //界面可见
+        else if (position <= 1) {
+            mRot = (ROT_MAX * position);
+            ViewCompat.setPivotX(view, view.getMeasuredWidth() * 0.1f);
+            ViewCompat.setPivotY(view, view.getMeasuredHeight());
+            ViewCompat.setRotation(view, mRot);
+            Log.i("md","2222222222222");
 
         }
         //界面不可见
